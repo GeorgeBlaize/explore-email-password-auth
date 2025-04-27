6c95a50090f7bd5b3c3237bf688d952e7dcfbf13
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-04-27 16:56:02 +0600

    added toggle show password and accepts terms

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,20 +1,28 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const SignUp = () => {
     const [success,setSuccess] = useState(false);
     const [errorMessage,setErrorMessage] =useState('')
+    const [showPassword,setShowPassword] = useState(false);
 
     const handleSignUp = e =>{
         e.preventDefault();
         const email = e.target.email.value;
         const password =e.target.password.value;
-        console.log(email,password);
+        const terms =e.target.terms.checked;
+        console.log(email,password,terms);
 
 
         setSuccess(false);
         setErrorMessage('');
+
+        if(!terms){
+            setErrorMessage('Please accepts Our Terms and Conditions');
+            return 
+        }
 
         //password validate
         const passwordRegExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
@@ -39,14 +47,30 @@ const SignUp = () => {
           
           <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-            <h1 className="text-3xl font-bold">Pleasre Sign Up now!</h1>
+            <h1 className="text-3xl font-bold">Please Sign Up now!</h1>
               <form onSubmit={handleSignUp}>
                 <label className="label">Email</label>
                 <input type="email" name='email' className="input" placeholder="Email" />
                 <label className="label mt-4">Password</label>
-                <input type="password" name='password' className="input" placeholder="Password" />
+                <div className='relative'>
+                <input type={showPassword ? 'text': 'password'}
+                    name='password'
+                     className="input" 
+                     placeholder="Password" />
+                <button onClick={() =>{setShowPassword(!showPassword)}} className='btn btn-xs absolute top-2 right-6'>
+                    {
+                        showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                    }
+                    </button>
+                </div>
                 <div><a className="link link-hover">Forgot password?</a></div>
-                <button className="btn btn-neutral mt-4">Login</button>
+                <label className="label mt-2">
+    <input type="checkbox" name="terms" defaultChecked className="checkbox" />
+    Accept Terms and Conditions
+  </label> 
+
+  <br />
+                <button className="btn btn-neutral mt-4">Sign Up</button>
               </form>
               {
                 errorMessage && <p className='text-red-500'>{errorMessage}</p>
